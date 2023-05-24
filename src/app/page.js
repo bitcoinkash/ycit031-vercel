0cb6271332e15e83ad4a9c0ac45d14fe8f54f003
SUHAIL PARRY <Suhail.parry@mail.mcgill.ca>
2023-05-23 23:04:26 -0400

    connected to db

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,33 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { db } from "@vercel/postgres";
+import { createClient } from '@vercel/postgres';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-     Hello
-    </main>
-  )
+
+
+export default async function Home()
+{
+
+  const client = createClient();
+
+  await client.connect();
+
+  const { rows } = await client.sql`
+  SELECT name, message 
+  FROM guest_message`;
+
+  console.log(rows);
+
+
+  return <main className={styles.main}>
+     {
+      rows.map((row) => (
+        <div key={row.name}>
+          <h3>{row.name}</h3>
+          <p>{row.message}</p>
+        </div>
+      ))
+     }
+    </main>;
 }
+
